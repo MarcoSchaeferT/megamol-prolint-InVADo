1ff8a66cb8466bdfcee870bf5c430d18b9b2207b
MarcoSchaeferT <marco.schaefer@uni-tuebingen.de>
2023-08-07 16:02:04 +0200

    updated CMake of prolint

--- plugins/prolint/get_InVADo_InstallData.cmd
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/MarcoSchaeferT/InVADo_setup.git
-xcopy InVADo_setup\checkmol.exe  /E /H /C /I /Y
-xcopy InVADo_setup\checkmol_linux /E /H /C /I /Y
-xcopy InVADo_setup\server\ server\ /E /H /C /I /Y
